381de9ef656853537674ab087c79c8252fd27fa6
Pratham Ghumare <ghumareps615@gmail.com>
2026-02-20 10:43:05 +0530

    first comm

--- Project_2_netflix.py
+++ Project_2_netflix.py
@@ -1,102 +1,5 @@
-
-# class Account:
-#     def __init__(self, id, sign_in, subscription):
-#         self.id = id
-#         self.sign_in = sign_in
-#         self.subscription = subscription
-
-#     def log_in_detail(self):
-#         print("user", self.id, "logged in using", self.sign_in)
-
-#     def verified_user(self):
-#         print("user verified  ")
-
-#     def user_details(self):
-#         print("user id: ", self.id)
-#         print("subscription: ", self.subscription)
-
-
-# class Plans:
-#     def __init__(self, plan_id, plan_name, price):
-#         self.plan_id = plan_id
-#         self.plan_name = plan_name
-#         self.price = price
-
-#     def plan_details(self):
-#         print("plan: ", self.plan_name)
-#         print("price: ", self.price)
-
-#     def valid_plan(self):
-#         print("plan is active")
-
-# class Payments:
-#     def __init__(self, transaction_id, trans_date, amount):
-#         self.transaction_id = transaction_id
-#         self.trans_date = trans_date
-#         self.amount = amount
-
-#     def payment_detail(self):
-#         print(f"transaction ID:  ", self.transaction_id)
-#         print(f"date: ",self.trans_date)
-#         print(f"amount Paid: ", self.amount)
-
-#     def pymt_validation(self):
-#         print("Payment successful and validated")
-
-
-# class FilmsSeries:
-#     def __init__(self, series_id, release_date, title):
-#         self.series_id = series_id
-#         self.release_date = release_date
-#         self.title = title
-#         self.favourites = []
-
-#     def add_to_fav(self):
-#         self.favourites.append(self.title)
-#         print(self.title," added to favourites",)
-
-#     def watched_fav(self):
-#         print("Watching: ",self.title)
-
-
-
-
-# acc1 = Account(1, "pratham@gmail.com", "Premium")
-# plan1 = Plans(101, "Premium", 799)
-# pmt1 = Payments(5001, "12-02-2026", 799)
-# movie = FilmsSeries(301, "2025", "Stranger Things")
-
-
-
-
-# acc1.log_in_detail()
-# acc1.verified_user()
-# acc1.user_details()
-
-# plan1.plan_details()
-# plan1.valid_plan()
-
-# pmt1.payment_detail()
-# pmt1.pymt_validation()
-
-# movie.add_to_fav()
-# movie.watched_fav()
-
-
-
-
-
-
-
-
-
-##Same project doing with SQL connectity
-
-
 import pyodbc
 from datetime import datetime
-
-# ---------------- DATABASE CONNECTION ----------------
 
 conn = pyodbc.connect(
     "Driver={SQL Server};"
@@ -108,7 +11,6 @@ conn = pyodbc.connect(
 cursor = conn.cursor()
 
 
-# ---------------- ACCOUNT CLASS ----------------
 class Account:
     def __init__(self, email):
         self.email = email
@@ -131,8 +33,6 @@ class Account:
             print("Email already registered!")
             return None
 
-
-# ---------------- PLAN CLASS ----------------
 class Plans:
     def __init__(self, plan_name):
         self.plan_name = plan_name
@@ -146,7 +46,6 @@ class Plans:
             return None
 
 
-# ---------------- PAYMENT CLASS ----------------
 class Payments:
     def __init__(self, user_id, amount):
         self.user_id = user_id
@@ -162,7 +61,6 @@ class Payments:
         print("Payment Successful!")
 
 
-# ---------------- MOVIES CLASS ----------------
 class Movies:
     def show_movies(self):
         cursor.execute("SELECT movie_id, title FROM Movies")
@@ -183,11 +81,10 @@ class Movies:
             conn.commit()
             print(result[0], "added to favourites!")
         else:
-            print("Invalid Movie Selection!")
+            print("invalid Movie Selection!")
 
 
-# ---------------- MAIN NETFLIX FLOW ----------------
-print("Welcome to Netflix\n")
+print("welcome to Netflix\n")
 
 email = input("enter your email: ")
 
@@ -216,7 +113,6 @@ else:
         exit()
 
 
-# ---------------- MOVIE SECTION ----------------
 movies = Movies()
 
 while True:
@@ -229,8 +125,7 @@ while True:
         break
 
 
-# ---------------- RECEIPT ----------------
-with open("acknowledgement.txt", "a") as file:
+with open("acknowledgement.txt", "a") as file:   #ad acknowledgement for priting user details
     file.write("----- NETFLIX RECEIPT -----\n")
     file.write(f"Email: {email}\n")
     file.write(f"Login Time: {datetime.now()}\n")
